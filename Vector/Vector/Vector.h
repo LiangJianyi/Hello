e83968ba6404ade13298499dd21d6df66fdcc82e
梁建溢 <liangjianyi.com@outlook.com>
2019-02-15 09:05:22 -0500

    _vector_concat(T,N1,N2)

--- Vector/Vector/Vector.h
+++ Vector/Vector/Vector.h
@@ -1,9 +1,14 @@
-#pragma once
+﻿#pragma once
+#include <stdlib.h>
 #include <memory.h>
+
+// 空字符串
 #define STRING_EMPTY ""
 
-#define _array_length(arr) sizeof(arr) / sizeof((arr)[0])
+// 获得数组或 vector 的长度
+#define _vector_length(arr) sizeof(arr) / sizeof((arr)[0])
 
+// 给指定的 vector 添加一个元素
 #define _add_vector(T,N) \
 T(*Add(T(*oldarr)[N],T value))[N + 1] { \
 	T(*newarr)[N + 1] = (T(*)[N + 1])malloc(sizeof(T[N + 1])); \
@@ -15,12 +20,34 @@ T(*Add(T(*oldarr)[N],T value))[N + 1] { \
 	} \
 }
 
-#define _concat_arr(T,N,L) \
- T (*ConcatArr(T(*newarr)[N] , T(*oldarr)[L]))[N] { \
+#define _vector_concat(T,N1,N2) \
+T(*VectorConcat(T(*vec1)[N1], T(*vec2)[N2]))[N1 + N2] { \
+	if (vec1 != NULL && vec2 != NULL) { \
+		STUFF(*newvector)[N1 + N2] = (STUFF(*)[N1 + N2])malloc(sizeof(STUFF[N1 + N2])); \
+		size_t i = 0; \
+		while (i < _vector_length(*vec1)) { \
+			(*newvector)[i] = (*vec1)[i]; \
+			i += 1; \
+		} \
+		do { \
+			(*newvector)[i] = (*vec2)[i]; \
+			i += 1; \
+		} while (i < _vector_length(*vec2)); \
+		return newvector; \
+	} \
+	else { \
+		abort(); \
+	} \
+}
+
+
+// 把一个 vector 的内容拷贝到另一个空 vector
+#define _vector_copy(T,N,L) \
+ T(*ArrayCopy(T(*newarr)[N] , T(*oldarr)[L]))[N] { \
 	if (newarr != NULL && oldarr != NULL) { \
-		size_t olen = _array_length(*oldarr); \
-		size_t nlen = _array_length(*newarr); \
-		if (nlen > olen) { \
+		size_t olen = _vector_length(*oldarr); \
+		size_t nlen = _vector_length(*newarr); \
+		if (nlen >= olen) { \
 			size_t i = 0; \
 			while (i < olen) { \
 				(*newarr)[i] = (*oldarr)[i]; \
@@ -42,7 +69,7 @@ T(*Add(T(*oldarr)[N],T value))[N + 1] { \
 	} \
 }
 
-
+// 创建一个 vector
 #define _create_vector(T,N) \
 T(*CreateVector())[N]{ \
 	return (T(*)[N])malloc(sizeof(T[N])); \
